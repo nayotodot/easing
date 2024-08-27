c91587d6b0c566e749bbaced569727ec8ddd1dac
NAYOTO. <17558264+nayotodot@users.noreply.github.com>
2024-08-27 16:49:45 +0900

    Misspell fixed

--- easing/inoutbounce.lua
+++ easing/inoutbounce.lua
@@ -1,7 +1,7 @@
 -- InOutBounce
 local outbounce = require "easing.outbounce";
 
-function inoutbounce(x)
+local function inoutbounce(x)
 	return x < 0.5 and ((1 - outbounce(1 - 2 * x)) / 2) or ((1 + outbounce(2 * x - 1)) / 2);
 end
 
